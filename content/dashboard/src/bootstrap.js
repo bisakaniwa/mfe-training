179adf039d61c4c1fd3c04d41208d0634fa998ce
Bianca Sakaniwa <bih_sayuri@hotmail.com>
2024-04-12 14:04:02 -0300

    added dashboard

--- content/dashboard/src/bootstrap.js
+++ content/dashboard/src/bootstrap.js
@@ -2,35 +2,18 @@ import { createApp } from 'vue';
 import Dashboard from './components/Dashboard.vue';
 
 // Mount function to start the app
-const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
-    const history = defaultHistory || createMemoryHistory({
-        initialEntries: [initialPath],
-    });
-
-    if (onNavigate) {
-        history.listen(onNavigate);
-    };
-
-    ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
-
-    return {
-        onParentNavigate({ pathname: nextPathname }) {
-            const { pathname } = history.location;
-
-            if (pathname !== nextPathname) {
-                history.push(nextPathname);
-            };
-        },
-    };
+const mount = (el) => {
+    const app = createApp(Dashboard);
+    app.mount(el);
 };
 
 // If running in development and in isolation, call mount immediately
 // Simplest of js to be able to communicate w/ diff frameworks
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_auth-dev-root');
+    const devRoot = document.querySelector('#_dashboard-dev-root');
 
     if (devRoot) {
-        mount(devRoot, { defaultHistory: createBrowserHistory() });
+        mount(devRoot);
     };
 };
 
